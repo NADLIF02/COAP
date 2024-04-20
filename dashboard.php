90b7418240156fe7a077bd272d3fd0c560090707
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-20 23:03:38 +0200

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,1 +1,13 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit;
+}
+
+echo "<h1>Welcome to the Dashboard!</h1>";
+echo "<a href='logout.php'>Logout</a> | <a href='view_requests.php'>View Leave Requests</a><br><br>";
+
+include('leave_request_form.html');
+?>
 
